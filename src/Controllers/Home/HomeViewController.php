bc1dec625f2052e0094047db5caa5a4bf333eb75
MatiasCirulli <lococirulli23@gmail.com>
2025-10-02 20:27:34 -0300

    fixed a syntax error

--- src/Controllers/Home/HomeViewController.php
+++ src/Controllers/Home/HomeViewController.php
@@ -2,7 +2,7 @@
 
 include_once $_SERVER["DOCUMENT_ROOT"].'/src/Controllers/ViewController.php';
 
-final readonly class AdminLoginViewController extends ViewController{
+final readonly class HomeViewController extends ViewController{
 
 
     public function __construct(){
