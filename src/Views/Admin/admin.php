5b266393beaf597f9b97707c828a8c3e6b578b56
Melina Masciulli <melinamasciulli@gmail.com>
2025-10-03 14:30:25 -0300

    modificacion de header

--- src/Views/Admin/admin.php
+++ src/Views/Admin/admin.php
@@ -291,12 +291,10 @@
     <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
-        <span class="d-none d-lg-block">Mi Panel</span>
-     </a>
-
       <i class="bi bi-list toggle-sidebar-btn"></i>
         <img src="logoUTN.png" alt="Logo" style="height:90px; width:auto;">
+         <a href="index.html" class="logo d-flex align-items-center">
+     </a>
         
     </div>
 
