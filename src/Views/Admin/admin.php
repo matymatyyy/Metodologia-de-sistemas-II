981d8e43c649db354b6acb760648c349e7954941
Melina Masciulli <melinamasciulli@gmail.com>
2025-10-03 14:21:49 -0300

    Logo UTN y logo usuarios incorporados en el panel adm

--- src/Views/Admin/admin.php
+++ src/Views/Admin/admin.php
@@ -293,8 +293,11 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <span class="d-none d-lg-block">Mi Panel</span>
-      </a>
+     </a>
+
       <i class="bi bi-list toggle-sidebar-btn"></i>
+        <img src="logoUTN.png" alt="Logo" style="height:90px; width:auto;">
+        
     </div>
 
         <nav class="header-nav ms-auto">
@@ -302,7 +305,7 @@
                 <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="https://via.placeholder.com/36" alt="Profile" class="rounded-circle">
+            <img src="usuario.png" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2">Usuario</span>
           </a>
 
